ab2949ef84b20eda450866b11918e2a2c3bc775f
Dassel <g.dassel@concat-solution.de>
2020-03-29 18:15:40 +0200

    Projektdateien hinzufügen.

---
+++ ReadCsvFileContent/Covid19Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadCsvFileContent
+{
+    public class Covid19Model
+    {
+        public string UrlConfirmed { get; set; } = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+        public string UrlDeaths { get; set; } = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv";
+        public string UrlRecovered { get; set; } = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv";
+        public string Country { get; set; } = "Germany";
+        public string Delimiter { get; set; } = ",";
+        public int Today { get; set; }
+        public int DayBefore { get; set; }
+        public int DayBeforeDiff { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
---
+++ ReadCsvFileContent/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace ReadCsvFileContent
+{
+    class Program
+    {
+        private static Covid19Model model = new Covid19Model();
+
+        static void Main(string[] args)
+        {
+            var confirmed = Covid19(model.UrlConfirmed);
+            Console.WriteLine($"Bestätigte Fälle: { confirmed.Today } ({ confirmed.DayBeforeDiff })");
+
+            var deaths = Covid19(model.UrlDeaths);
+            Console.WriteLine($"Todesfälle: { deaths.Today } ({ deaths.DayBeforeDiff })");
+
+            var recovered = Covid19(model.UrlRecovered);
+            Console.WriteLine($"Genesen: { recovered.Today } ({ recovered.DayBeforeDiff })");
+
+            Console.ReadLine();
+        }
+
+        private static Covid19Model Covid19(string url)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                Stream stream = client.OpenRead(url);
+                StreamReader reader = new StreamReader(stream);
+                string line;
+                
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] splitted = line.Split(model.Delimiter);
+                    if (splitted[1] == model.Country)
+                    {
+                        model.DayBefore = Int32.Parse(splitted[splitted.Length - 2]);
+                        model.Today = Int32.Parse(splitted[splitted.Length - 1]);
+                        model.DayBeforeDiff = model.Today - model.DayBefore;
+                        
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                model.ErrorMessage = $"Fehler: { ex.Message }";
+                return model;
+            }
+
+            return model;
+        }
+    }
+}
